75aeba2c38a8324216c02746bfcbf1aa9091c6f6
Ziqi Ling <ziqi.ling@mail.utoronto.ca>
2020-09-28 00:51:55 +0000

    First Commit

--- Setup.R
+++ Setup.R
@@ -6,7 +6,7 @@ blogdown::install_hugo()
 update_hugo()
 hugo_version()
 file.create("netlify.toml")
-blogdown::new_site(  dir = "b",
+blogdown::new_site(  dir = "a",
                      install_hugo = TRUE,
                      format = "toml",
                      sample = TRUE,
